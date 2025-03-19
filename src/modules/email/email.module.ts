48c677de3df2d815c0af39badee1c18b9e235d70
Breno Baldassim <brenobaldassim.contato@gmail.com>
2025-03-19 19:41:31 -0300

    feat: email service done

---
+++ src/modules/email/email.module.ts
@@ -0,0 +1,50 @@
+import * as path from 'path';
+import { Module } from '@nestjs/common';
+import { EmailService } from './services/email.service';
+import { UserModule } from '../user/user.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EmailServiceContract } from './contracts/email.service.contract';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
+@Module({
+  imports: [
+    PrismaModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('EMAIL_HOST'),
+          port: Number(configService.get('EMAIL_PORT')),
+          secure: false,
+          auth: {
+            user: configService.get('EMAIL_USER'),
+            pass: configService.get('EMAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"No Reply" <${configService.get('EMAIL_FROM')}>`,
+        },
+        template: {
+          dir: path.join(__dirname, 'templates'),
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: EmailServiceContract,
+      useClass: EmailService,
+    },
+  ],
+  exports: [EmailServiceContract],
+})
+export class EmailModule {}
---
+++ src/modules/email/services/email.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import {
+  EmailServiceContract,
+  EmailServiceSendInput,
+} from '../contracts/email.service.contract';
+
+@Injectable()
+export class EmailService implements EmailServiceContract {
+  constructor(
+    private readonly mailService: MailerService,
+    private readonly configService: ConfigService,
+  ) {}
+  public async send(input: EmailServiceSendInput): Promise<void> {
+    await this.mailService.sendMail({
+      ...input,
+      from: this.configService.get('EMAIL_FROM'),
+    });
+  }
+
+  public async sendResetPasswordEmail(email: string, token: string) {
+    const tokenUrl = `${this.configService.get('EMAIL_RESET_PASSWORD_URL')}?token=${token}`;
+
+    await this.send({
+      to: email,
+      subject: 'Reset your password',
+      template: 'reset-password',
+      context: {
+        token: tokenUrl,
+      },
+    });
+  }
+
+  public async sendWelcomeEmail(email: string) {
+    await this.send({
+      to: email,
+      subject: 'Welcome to the app',
+      template: 'welcome',
+      context: {},
+    });
+  }
+}
