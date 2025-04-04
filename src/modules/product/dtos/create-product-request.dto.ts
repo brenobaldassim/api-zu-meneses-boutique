0b6c1c27efec163fbda511eb455bcc8828fe421f
Breno Baldassim <brenobaldassim.contato@gmail.com>
2025-04-03 23:12:35 -0300

    min as 0 for price an quantity

--- src/modules/product/dtos/create-product-request.dto.ts
+++ src/modules/product/dtos/create-product-request.dto.ts
@@ -1,15 +1,19 @@
 import { Type } from 'class-transformer';
-import { IsInt, IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty, Min } from 'class-validator';
 
 export class CreateProductRequestDto {
   @IsNotEmpty({ message: 'Name cannot be empty' })
   name: string;
+
   @IsNotEmpty({ message: 'Price cannot be empty' })
   @Type(() => Number)
   @IsInt()
+  @Min(0)
   priceCents: number;
+
   @IsNotEmpty({ message: 'Quantity cannot be empty' })
   @Type(() => Number)
   @IsInt()
+  @Min(0)
   quantity: number;
 }
