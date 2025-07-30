f5a62b735f49165b8d7c7062665e5b5e263d49db
Breno Baldassim <brenobaldassim.contato@gmail.com>
2025-07-29 23:16:14 -0300

    fix

--- src/modules/@server/main.ts
+++ src/modules/@server/main.ts
@@ -7,6 +7,13 @@ const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [process.env.APP_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
 }
